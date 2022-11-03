77f8ee019cd0336be0825efea2f048f8e0ec05cb
DianaD02 <dragut.diana02@gmail.com>
2022-11-03 21:33:04 +0200

    Lab 4

--- Pages/Books/Create.cshtml.cs
+++ Pages/Books/Create.cshtml.cs
@@ -21,14 +21,8 @@ namespace Dragut_Diana_Lab2.Pages.Books
 
         public IActionResult OnGet()
         {
-            var authorList = _context.Author.Select(x => new
-            {
-                x.Id,
-                FullName = x.LastName + " " + x.FirstName
-            });
-
-            ViewData["AuthorID"] = new SelectList(authorList, "ID", "FullName");
-            ViewData["PublisherID"] = new SelectList(_context.Publisher, "ID", "PublisherName");
+            ViewData["AuthorID"] = new SelectList(_context.Set<Author>(), "ID", "FullName");
+            ViewData["PublisherID"] = new SelectList(_context.Set<Publisher>(), "ID", "PublisherName");
 
             var book = new Book();
             book.BookCategory = new List<BookCategory>();
--- Pages/Publishers/Index.cshtml.cs
+++ Pages/Publishers/Index.cshtml.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using Dragut_Diana_Lab2.Data;
 using Dragut_Diana_Lab2.Models;
+using Dragut_Diana_Lab2.Models.ViewModels;
 
 namespace Dragut_Diana_Lab2.Pages.Publishers
 {
@@ -21,11 +22,24 @@ namespace Dragut_Diana_Lab2.Pages.Publishers
 
         public IList<Publisher> Publisher { get;set; } = default!;
 
-        public async Task OnGetAsync()
-        {
-            if (_context.Publisher != null)
+        public PublisherIndexData PublisherData { get; set; }
+        public int PublisherID { get; set; }
+        public int BookID { get; set; }
+        public async Task OnGetAsync(int? id, int? bookID )
+        {   PublisherData=new PublisherIndexData();
+            PublisherData.Publishers=await _context.Publisher
+                .Include(i=> i.Books)
+                   .ThenInclude(c =>c.Author)
+                .OrderBy(i=>i.PublisherName)
+                .ToListAsync();
+            if (id != null)
             {
-                Publisher = await _context.Publisher.ToListAsync();
+                PublisherID = id.Value;
+                Publisher publishers = PublisherData.Publishers
+                    .Where(i => i.ID == id.Value).Single();
+                PublisherData.Books = publishers.Books;
+
+
             }
         }
     }
